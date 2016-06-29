fb2e5d36709c71f58ba58602b4faef6a8740b2e7
Vincent Ramdhanie <vramdhanie@gmail.com>
2016-06-29 13:51:09 -0400

    Initial refactor for MVC

--- js/index.js
+++ js/index.js
@@ -81,6 +81,7 @@ var Question = function (name, text, mystery, actual, choices, answer,
     this.choices = choices || [];
     this.answer = answer || null;
     this.feedback = feedback || "";
+    this.text = text || "";
 };
 
 /**
@@ -105,163 +106,237 @@ function createQuestion(data) {
 // Store the question instances in a variable as an array.
 var questions = createQuestion(data);
 
+var Model = function(questions){
+    this.currentQuestion = 2;
+    this.score = 0;
+    this.questions = questions;
+}
+
+Model.prototype.increment = function(){
+    this.currentQuestion += 1;
+}
+
+Model.prototype.getCurrentQuestion = function(){
+    return this.questions[this.currentQuestion];
+}
+
+Model.prototype.reset = function(){
+    this.score = 0;
+    this.currentQuestion = 0;
+}
+
+
+/**
+ * Create a View object
+ */
+function View(){
+    //declare elements such as question containers
+    this.question = $('.question');
+    this.nextBtn = $('.nextBtn');
+    this.responseList = $('.response-list');
+
+    this.next = null;
+
+    this.nextBtn.on('click', this.onNext.bind(this));
+
+}
+
+View.prototype.onNext = function(){
+
+}
+
+View.prototype.template = function(i, text){
+    return '<li class="response">' +
+        '<input type="radio" name="selection" value="' + i + '">' +
+        '<span>' + text + '</span>' +
+        '</li>';
+}
+
+View.prototype.displayQuestion = function(question){
+    //code to display a question
+    this.question.html(question.text);
+    //code to display all options
+    var choices = question.choices;
+    this.responseList.empty();
+    for (var i=0; i < choices.length; i++) {
+        this.responseList.append(this.template(i, choices[i]));
+    }
+}
+
+function Controller(model, view){
+    this.model = model;
+    this.view = view;
+
+    //set up bindings
+}
+
+Controller.prototype.updateDisplay = function(){
+    this.view.displayQuestion(this.model.getCurrentQuestion());
+}
 
 
 
 $(document).ready(function () {
 
+    var view = new View();
+    var model = new Model(questions);
+    var controller = new Controller(model, view);
+    controller.updateDisplay();
 
-    // initialize variables and start the quiz.
-    var questionNumber;
-    startQuiz();
+    //view.next = model.increment.bind(model);
 
-    function startQuiz() {
-        questionNumber = 0;
-        // Go to the top of the page.
-        scrollToTop();
-        resetScore();
-        setImage('mystery');
-        setButton('submit');
-        updateQuestion();
-    }
 
-    $('.button').on('click', '#submit-button', function (e) {
-        // Prevent the page from reloading.
-        e.preventDefault();
-        // Check the response and update the score.
-        var judgement;
-        var response = checkResponse();
-        if (response) {
-            judgement = "Well done!";
-        } else {
-            judgement = "Nice try!";
-        }
-        // Set the feedback to present to the user.
-        var feedback = questions[questionNumber].feedback;
-        feedback = judgement + " " + feedback;
-        // Hide the list of choices and display the feedback.
-        $('ul.response-list').hide();
-        $('div.answer').html(
-            '<span>Answer: </span><p class="answer-text">' +
-            feedback + '</p>');
-        $('div.answer').show();
-        // Set the current image to the actual image.
-        setImage('actual');
-        // Change the button depending on where the user is in the quiz.
-        if (questionNumber < 4) {
-            setButton('next');
-        } else {
-            setButton('retake');
-        }
-        // Go to the top of the page - for smaller displays.
-        scrollToTop();
-
-    });
-
-    $('.button').on('click', '#next-button', function (e) {
-        // Prevent the button event from reloading the page.
-        e.preventDefault();
-        // Get the new question and answer choices.
-        // Increment the question number.
-        questionNumber += 1;
-        updateQuestion();
-        // Update the image.
-        setImage('mystery');
-        // Show the submit button.
-        setButton('submit');
-        // Go to the top of the page - for smaller displays.
-        scrollToTop();
-
-    });
-
-    $('.button').on('click', '#retake-button', function (e) {
-        // Prevent the page reload.
-        e.preventDefault();
-        startQuiz();
-    });
-
-    function setImage(type) {
-        var image;
-        if (type === 'actual') {
-            // Set the current image to the actual image.
-            image = questions[questionNumber].actualImg;
-        } else if (type === 'mystery') {
-            // Set the image to the mystery image.
-            image = questions[questionNumber].mysteryImg;
-        }
-        $('.player img').attr('src', image);
-        return;
-    }
-
-    function setButton(value) {
-        var submitHTML =
-            '<input type="submit" id="submit-button" value="Submit" name="submit" />';
-        var nextHTML =
-            '<input type="submit" id="next-button" value="Next" name="next" />';
-        var retakeHTML =
-            '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
-        if (value === 'submit') {
-            $('.button').html(submitHTML);
-        } else if (value === 'next') {
-            $('.button').html(nextHTML);
-        } else if (value === 'retake') {
-            $('.button').html(retakeHTML);
-        }
-        return;
-    }
-
-    function updateQuestion() {
-        var questionText = questions[questionNumber].text;
-        var choices = questions[questionNumber].choices;
-        var number = questionNumber + 1;
-
-        // Insert the new question text and the list of choices into the page.
-        $('.question').html(
-            '<span>Question </span><span class="count">' + number +
-            '</span>: <p class="answer-text">' + questionText +
-            '</p>');
-        $('ul.response-list').empty();
-        $('div.answer').hide();
-        $('ul.response-list').show();
-        for (var i = 0; i < choices.length; i++) {
-            $('ul.response-list').append(
-                '<li class="response"><input type="radio" name="selection" value="' +
-                i + '"><span>' + choices[i] + '</span></li>');
-        }
-        return;
-    }
-
-    function checkResponse() {
-        // Add a few variables that will be used to render the correct
-        // score ball depending on the user's response to the question.
-        var choice = $("input[type='radio'][name='selection']:checked")
-            .val();
-        var correctAnswer = questions[questionNumber].answer;
-        var answer;
-        var ball;
-        // Check the response to see if it's right or wrong.
-        if (choice == correctAnswer) {
-            ball = 'images/correct-answer-ball.png';
-            answer = true;
-        } else {
-            ball = 'images/wrong-answer-ball.png';
-            answer = false;
-        }
-        $('li.ball-' + questionNumber + ' img').attr('src', ball);
-        return answer;
-    }
-
-    function resetScore() {
-        $('ul.tennis-balls').empty();
-        for (var i = 0; i < questions.length; i++) {
-            $('ul.tennis-balls').append('<li class="score-ball ball-' +
-                i +
-                '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>'
-            );
-        }
-    }
-
-    function scrollToTop() {
-        $(document).scrollTop(0);
-    }
+    // // initialize variables and start the quiz.
+    // var questionNumber;
+    // startQuiz();
+    //
+    // function startQuiz() {
+    //     questionNumber = 0;
+    //     // Go to the top of the page.
+    //     scrollToTop();
+    //     resetScore();
+    //     setImage('mystery');
+    //     setButton('submit');
+    //     updateQuestion();
+    // }
+    //
+    // $('.button').on('click', '#submit-button', function (e) {
+    //     // Prevent the page from reloading.
+    //     e.preventDefault();
+    //     // Check the response and update the score.
+    //     var judgement;
+    //     var response = checkResponse();
+    //     if (response) {
+    //         judgement = "Well done!";
+    //     } else {
+    //         judgement = "Nice try!";
+    //     }
+    //     // Set the feedback to present to the user.
+    //     var feedback = questions[questionNumber].feedback;
+    //     feedback = judgement + " " + feedback;
+    //     // Hide the list of choices and display the feedback.
+    //     $('ul.response-list').hide();
+    //     $('div.answer').html(
+    //         '<span>Answer: </span><p class="answer-text">' +
+    //         feedback + '</p>');
+    //     $('div.answer').show();
+    //     // Set the current image to the actual image.
+    //     setImage('actual');
+    //     // Change the button depending on where the user is in the quiz.
+    //     if (questionNumber < 4) {
+    //         setButton('next');
+    //     } else {
+    //         setButton('retake');
+    //     }
+    //     // Go to the top of the page - for smaller displays.
+    //     scrollToTop();
+    //
+    // });
+    //
+    // $('.button').on('click', '#next-button', function (e) {
+    //     // Prevent the button event from reloading the page.
+    //     e.preventDefault();
+    //     // Get the new question and answer choices.
+    //     // Increment the question number.
+    //     questionNumber += 1;
+    //     updateQuestion();
+    //     // Update the image.
+    //     setImage('mystery');
+    //     // Show the submit button.
+    //     setButton('submit');
+    //     // Go to the top of the page - for smaller displays.
+    //     scrollToTop();
+    //
+    // });
+    //
+    // $('.button').on('click', '#retake-button', function (e) {
+    //     // Prevent the page reload.
+    //     e.preventDefault();
+    //     startQuiz();
+    // });
+    //
+    // function setImage(type) {
+    //     var image;
+    //     if (type === 'actual') {
+    //         // Set the current image to the actual image.
+    //         image = questions[questionNumber].actualImg;
+    //     } else if (type === 'mystery') {
+    //         // Set the image to the mystery image.
+    //         image = questions[questionNumber].mysteryImg;
+    //     }
+    //     $('.player img').attr('src', image);
+    //     return;
+    // }
+    //
+    // function setButton(value) {
+    //     var submitHTML =
+    //         '<input type="submit" id="submit-button" value="Submit" name="submit" />';
+    //     var nextHTML =
+    //         '<input type="submit" id="next-button" value="Next" name="next" />';
+    //     var retakeHTML =
+    //         '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
+    //     if (value === 'submit') {
+    //         $('.button').html(submitHTML);
+    //     } else if (value === 'next') {
+    //         $('.button').html(nextHTML);
+    //     } else if (value === 'retake') {
+    //         $('.button').html(retakeHTML);
+    //     }
+    //     return;
+    // }
+    //
+    // function updateQuestion() {
+    //     var questionText = questions[questionNumber].text;
+    //     var choices = questions[questionNumber].choices;
+    //     var number = questionNumber + 1;
+    //
+    //     // Insert the new question text and the list of choices into the page.
+    //     $('.question').html(
+    //         '<span>Question </span><span class="count">' + number +
+    //         '</span>: <p class="answer-text">' + questionText +
+    //         '</p>');
+    //     $('ul.response-list').empty();
+    //     $('div.answer').hide();
+    //     $('ul.response-list').show();
+    //     for (var i = 0; i < choices.length; i++) {
+    //         $('ul.response-list').append(
+    //             '<li class="response"><input type="radio" name="selection" value="' +
+    //             i + '"><span>' + choices[i] + '</span></li>');
+    //     }
+    //     return;
+    // }
+    //
+    // function checkResponse() {
+    //     // Add a few variables that will be used to render the correct
+    //     // score ball depending on the user's response to the question.
+    //     var choice = $("input[type='radio'][name='selection']:checked")
+    //         .val();
+    //     var correctAnswer = questions[questionNumber].answer;
+    //     var answer;
+    //     var ball;
+    //     // Check the response to see if it's right or wrong.
+    //     if (choice == correctAnswer) {
+    //         ball = 'images/correct-answer-ball.png';
+    //         answer = true;
+    //     } else {
+    //         ball = 'images/wrong-answer-ball.png';
+    //         answer = false;
+    //     }
+    //     $('li.ball-' + questionNumber + ' img').attr('src', ball);
+    //     return answer;
+    // }
+    //
+    // function resetScore() {
+    //     $('ul.tennis-balls').empty();
+    //     for (var i = 0; i < questions.length; i++) {
+    //         $('ul.tennis-balls').append('<li class="score-ball ball-' +
+    //             i +
+    //             '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>'
+    //         );
+    //     }
+    // }
+    //
+    // function scrollToTop() {
+    //     $(document).scrollTop(0);
+    // }
 });
