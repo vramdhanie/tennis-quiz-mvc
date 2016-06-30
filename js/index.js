1d7e2ccb22b58b1dd2df5b3def24f2e4a75792f8
David Conner <agilebluefox@gmail.com>
2016-06-29 23:14:50 -0400

    Added constructors for the MVC components.

--- js/index.js
+++ js/index.js
@@ -13,58 +13,58 @@ Some things to consider:
 // Array of question objects. Once I get this working I'll import these
 // from a separate file.
 var data = [{
-        text: 'which player on the atp tour has won the most grand slam titles?',
+        text: 'Which player on the ATP Tour has won the most Grand Slam titles?',
         mystery_img: 'images/mystery-federer.png',
         actual_img: 'images/federer.png',
-        choices: ['pete sampras', 'rafael nadal',
-            'roger federer', 'john mcEnroe'
+        choices: ['Pete Sampras', 'Rafael Nadal',
+            'Roger Federer', 'John McEnroe'
         ],
         answer: 2,
-        feedback: 'roger federer has won 17 grand slam titles including 7 wimbledon, 5 us open, 1 french open, and 4 australian open titles.'
+        feedback: 'Roger Federer has won 17 grand slam titles including 7 Wimbledon, 5 US Open, 1 French Open, and 4 Australian Open titles.'
     },
 
     {
-        text: 'this player has won the french open a whopping nine times over a ten year period!',
+        text: 'This player has won the French Open a whopping nine times over a ten year period!',
         mystery_img: 'images/mystery-nadal.png',
         actual_img: 'images/nadal.png',
-        choices: ['roger federer', 'rafael nadal',
-            'novak djokovic', 'john isner'
+        choices: ['Roger Federer', 'Rafael Nadal',
+            'Novak Djokovic', 'John Isner'
         ],
         answer: 1,
-        feedback: 'rafael nadal is considered to be "the king of clay" and one of the greatest players ever.'
+        feedback: 'Rafael Nadal is considered to be "the King of Clay" and one of the greatest players ever.'
     },
 
     {
-        text: 'which of these players won the men\'s singles championship at the 2012 olympic games?',
+        text: 'Which of these players won the men\'s singles championship at the 2012 Olympic Games?',
         mystery_img: 'images/mystery-murray.png',
         actual_img: 'images/murray.png',
-        choices: ['andy murray', 'novak djokovic',
-            'pete sampras', 'andy roddick'
+        choices: ['Andy Murray', 'Novak Djokovic',
+            'Pete Sampras', 'Andy Roddick'
         ],
         answer: 0,
-        feedback: 'andy murray is the first british singles champion in over 100 years.'
+        feedback: 'Andy Murray is the first British singles champion in over 100 years.'
     },
 
     {
-        text: 'who is the only player to have beaten both roger federer and rafael nadal in all four grand slam events?',
+        text: 'Who is the only player to have beaten both Roger Federer and Rafael Nadal in all four Grand Slam events?',
         mystery_img: 'images/mystery-djokovic.png',
         actual_img: 'images/djokovic.png',
-        choices: ['andre agassi', 'andy roddick',
-            'novak djokovic', 'pete sampras'
+        choices: ['Andre Agassi', 'Andy Roddick',
+            'Novak Djokovic', 'pete sampras'
         ],
         answer: 2,
-        feedback: 'novak djokovic is currently ranked number 1 in the world on the atp tour.'
+        feedback: 'Novak Djokovic is currently ranked number 1 in the world on the ATP Tour.'
     },
 
     {
-        text: 'this player won the longest men\'s singles match ever to be played at wimbledon.',
+        text: 'This player won the longest men\'s singles match ever to be played at Wimbledon.',
         mystery_img: 'images/mystery-isner.png',
         actual_img: 'images/isner.png',
-        choices: ['rafael nadal', 'john mcEnroe',
-            'pete sampras', 'john isner'
+        choices: ['Rafael Nadal', 'John McEnroe',
+            'Pete Sampras', 'John Isner'
         ],
         answer: 3,
-        feedback: 'john isner defeated nicolaus mahut in 11 hours and 5 minutes. the match was played over a three day period since there are no court lights at wimbledon.'
+        feedback: 'John Isner defeated Nicolaus Mahut in 11 hours and 5 minutes. the match was played over a three day period since there are no court lights at Wimbledon.'
     }
 
 ];
@@ -76,6 +76,7 @@ var data = [{
 var Question = function (name, text, mystery, actual, choices, answer,
     feedback) {
     this.name = name || "";
+    this.text = text || "";
     this.mysteryImg = mystery || "";
     this.actualImg = actual || "";
     this.choices = choices || [];
@@ -88,7 +89,7 @@ var Question = function (name, text, mystery, actual, choices, answer,
  * @param  {array} data Array of questions as JSON objects.
  * @return {array} qList Array of question object instances.
  */
-function createQuestion(data) {
+function createQuestion (data) {
     var qList = [],
         count = 0;
     $.each(data, function (i, question) {
@@ -100,168 +101,249 @@ function createQuestion(data) {
         qList.push(q);
     });
     return qList;
-}
+};
 
 // Store the question instances in a variable as an array.
 var questions = createQuestion(data);
 
+/**
+ * Represents the data.
+ * @param {Array} questions Array of question instances.
+ */
+var Model = function (questions) {
+    this.questions = questions;
+    this.currentQuestion = 0;
+    this.score = 0;
+};
 
+// Method to increment the question number as the user progresses in the quiz.
+Model.prototype.increment = function() {
+    this.currentQuestion += 1;
+}
 
+// Method to retrieve a question from the data storage (array in this case).
+Model.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestion];
+}
 
-$(document).ready(function () {
+// Reset some of the properties if the user wants to retake the quiz.
+Model.prototype.reset = function () {
+    this.score = 0;
+    this.currentQuestion = 0;
+}
 
+/**
+ * Represents the view.
+ */
+var View = function () {
+    //declare the elements that need to be manipulated.
+    this.question = $('.question');
+    this.button = $('.button');
+    this.responseList = $('.response-list');
 
-    // initialize variables and start the quiz.
-    var questionNumber;
-    startQuiz();
+    this.next = null;
+}
 
-    function startQuiz() {
-        questionNumber = 0;
-        // Go to the top of the page.
-        scrollToTop();
-        resetScore();
-        setImage('mystery');
-        setButton('submit');
-        updateQuestion();
+View.prototype.template = function(number, text) {
+    return '<li class="response"><input type="radio" name="selection" value="' +
+                number + '"><span>' + text + '</span></li>'
+}
+
+View.prototype.onNext = function() {
+
+}
+
+View.prototype.displayQuestion = function (question) {
+    // code to display a question
+    this.question.text(question.text);
+    // display the choices
+    var choices = question.choices;
+    for (var i = 0; i < choices.length; i++) {
+        var option = choices[i];
+        this.responseList.html(this.template(i, option));
     }
+}
 
-    $('.button').on('click', '#submit-button', function (e) {
-        // Prevent the page from reloading.
-        e.preventDefault();
-        // Check the response and update the score.
-        var judgement;
-        var response = checkResponse();
-        if (response) {
-            judgement = "Well done!";
-        } else {
-            judgement = "Nice try!";
-        }
-        // Set the feedback to present to the user.
-        var feedback = questions[questionNumber].feedback;
-        feedback = judgement + " " + feedback;
-        // Hide the list of choices and display the feedback.
-        $('ul.response-list').hide();
-        $('div.answer').html(
-            '<span>Answer: </span><p class="answer-text">' +
-            feedback + '</p>');
-        $('div.answer').show();
-        // Set the current image to the actual image.
-        setImage('actual');
-        // Change the button depending on where the user is in the quiz.
-        if (questionNumber < 4) {
-            setButton('next');
-        } else {
-            setButton('retake');
-        }
-        // Go to the top of the page - for smaller displays.
-        scrollToTop();
+/**
+ * Handles the connection between the view and the model.
+ * @param {Object} model Stores and manipulates the data.
+ * @param {Object} view  Presents the data and listens for events.
+ */
+var Controller = function (model, view) {
+     this.model = model;
+     this.view = view;
+};
 
-    });
+// Take the data from the model and render it in the view.
+Controller.prototype.updateQuestion = function () {
+    this.view.displayQuestion(this.model.getCurrentQuestion());
+}
 
-    $('.button').on('click', '#next-button', function (e) {
-        // Prevent the button event from reloading the page.
-        e.preventDefault();
-        // Get the new question and answer choices.
-        // Increment the question number.
-        questionNumber += 1;
-        updateQuestion();
-        // Update the image.
-        setImage('mystery');
-        // Show the submit button.
-        setButton('submit');
-        // Go to the top of the page - for smaller displays.
-        scrollToTop();
+$(document).ready(startQuiz);
 
-    });
+// Instantiate the MVC components.
+function startQuiz () {
+    var model = new Model(questions);
+    var view = new View();
+    var controller = new Controller(model, view);
 
-    $('.button').on('click', '#retake-button', function (e) {
-        // Prevent the page reload.
-        e.preventDefault();
-        startQuiz();
-    });
+    // Get the quiz started.
+    controller.updateQuestion();
+}
 
-    function setImage(type) {
-        var image;
-        if (type === 'actual') {
-            // Set the current image to the actual image.
-            image = questions[questionNumber].actualImg;
-        } else if (type === 'mystery') {
-            // Set the image to the mystery image.
-            image = questions[questionNumber].mysteryImg;
-        }
-        $('.player img').attr('src', image);
-        return;
-    }
 
-    function setButton(value) {
-        var submitHTML =
-            '<input type="submit" id="submit-button" value="Submit" name="submit" />';
-        var nextHTML =
-            '<input type="submit" id="next-button" value="Next" name="next" />';
-        var retakeHTML =
-            '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
-        if (value === 'submit') {
-            $('.button').html(submitHTML);
-        } else if (value === 'next') {
-            $('.button').html(nextHTML);
-        } else if (value === 'retake') {
-            $('.button').html(retakeHTML);
-        }
-        return;
-    }
+//     // initialize variables and start the quiz.
+//     var currentQuestion;
+//     startQuiz();
 
-    function updateQuestion() {
-        var questionText = questions[questionNumber].text;
-        var choices = questions[questionNumber].choices;
-        var number = questionNumber + 1;
+//     function startQuiz() {
+//         questionNumber = 0;
+//         // Go to the top of the page.
+//         scrollToTop();
+//         resetScore();
+//         setImage('mystery');
+//         setButton('submit');
+//         updateQuestion();
+//     }
 
-        // Insert the new question text and the list of choices into the page.
-        $('.question').html(
-            '<span>Question </span><span class="count">' + number +
-            '</span>: <p class="answer-text">' + questionText +
-            '</p>');
-        $('ul.response-list').empty();
-        $('div.answer').hide();
-        $('ul.response-list').show();
-        for (var i = 0; i < choices.length; i++) {
-            $('ul.response-list').append(
-                '<li class="response"><input type="radio" name="selection" value="' +
-                i + '"><span>' + choices[i] + '</span></li>');
-        }
-        return;
-    }
 
-    function checkResponse() {
-        // Add a few variables that will be used to render the correct
-        // score ball depending on the user's response to the question.
-        var choice = $("input[type='radio'][name='selection']:checked")
-            .val();
-        var correctAnswer = questions[questionNumber].answer;
-        var answer;
-        var ball;
-        // Check the response to see if it's right or wrong.
-        if (choice == correctAnswer) {
-            ball = 'images/correct-answer-ball.png';
-            answer = true;
-        } else {
-            ball = 'images/wrong-answer-ball.png';
-            answer = false;
-        }
-        $('li.ball-' + questionNumber + ' img').attr('src', ball);
-        return answer;
-    }
+//     $('.button').on('click', '#submit-button', function (e) {
+//         // Prevent the page from reloading.
+//         e.preventDefault();
+//         // Check the response and update the score.
+//         var judgement;
+//         var response = checkResponse();
+//         if (response) {
+//             judgement = "Well done!";
+//         } else {
+//             judgement = "Nice try!";
+//         }
+//         // Set the feedback to present to the user.
+//         var feedback = questions[questionNumber].feedback;
+//         feedback = judgement + " " + feedback;
+//         // Hide the list of choices and display the feedback.
+//         $('ul.response-list').hide();
+//         $('div.answer').html(
+//             '<span>Answer: </span><p class="answer-text">' +
+//             feedback + '</p>');
+//         $('div.answer').show();
+//         // Set the current image to the actual image.
+//         setImage('actual');
+//         // Change the button depending on where the user is in the quiz.
+//         if (questionNumber < 4) {
+//             setButton('next');
+//         } else {
+//             setButton('retake');
+//         }
+//         // Go to the top of the page - for smaller displays.
+//         scrollToTop();
 
-    function resetScore() {
-        $('ul.tennis-balls').empty();
-        for (var i = 0; i < questions.length; i++) {
-            $('ul.tennis-balls').append('<li class="score-ball ball-' +
-                i +
-                '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>'
-            );
-        }
-    }
+//     });
 
-    function scrollToTop() {
-        $(document).scrollTop(0);
-    }
-});
+//     $('.button').on('click', '#next-button', function (e) {
+//         // Prevent the button event from reloading the page.
+//         e.preventDefault();
+//         // Get the new question and answer choices.
+//         // Increment the question number.
+//         questionNumber += 1;
+//         updateQuestion();
+//         // Update the image.
+//         setImage('mystery');
+//         // Show the submit button.
+//         setButton('submit');
+//         // Go to the top of the page - for smaller displays.
+//         scrollToTop();
+
+//     });
+
+//     $('.button').on('click', '#retake-button', function (e) {
+//         // Prevent the page reload.
+//         e.preventDefault();
+//         startQuiz();
+//     });
+
+//     function setImage(type) {
+//         var image;
+//         if (type === 'actual') {
+//             // Set the current image to the actual image.
+//             image = questions[questionNumber].actualImg;
+//         } else if (type === 'mystery') {
+//             // Set the image to the mystery image.
+//             image = questions[questionNumber].mysteryImg;
+//         }
+//         $('.player img').attr('src', image);
+//         return;
+//     }
+
+//     function setButton(value) {
+//         var submitHTML =
+//             '<input type="submit" id="submit-button" value="Submit" name="submit" />';
+//         var nextHTML =
+//             '<input type="submit" id="next-button" value="Next" name="next" />';
+//         var retakeHTML =
+//             '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
+//         if (value === 'submit') {
+//             $('.button').html(submitHTML);
+//         } else if (value === 'next') {
+//             $('.button').html(nextHTML);
+//         } else if (value === 'retake') {
+//             $('.button').html(retakeHTML);
+//         }
+//         return;
+//     }
+
+//     function updateQuestion() {
+//         var questionText = questions[questionNumber].text;
+//         var choices = questions[questionNumber].choices;
+//         var number = questionNumber + 1;
+
+//         // Insert the new question text and the list of choices into the page.
+//         $('.question').html(
+//             '<span>Question </span><span class="count">' + number +
+//             '</span>: <p class="answer-text">' + questionText +
+//             '</p>');
+//         $('ul.response-list').empty();
+//         $('div.answer').hide();
+//         $('ul.response-list').show();
+//         for (var i = 0; i < choices.length; i++) {
+//             $('ul.response-list').append(
+//                 '<li class="response"><input type="radio" name="selection" value="' +
+//                 i + '"><span>' + choices[i] + '</span></li>');
+//         }
+//         return;
+//     }
+
+//     function checkResponse() {
+//         // Add a few variables that will be used to render the correct
+//         // score ball depending on the user's response to the question.
+//         var choice = $("input[type='radio'][name='selection']:checked")
+//             .val();
+//         var correctAnswer = questions[questionNumber].answer;
+//         var answer;
+//         var ball;
+//         // Check the response to see if it's right or wrong.
+//         if (choice == correctAnswer) {
+//             ball = 'images/correct-answer-ball.png';
+//             answer = true;
+//         } else {
+//             ball = 'images/wrong-answer-ball.png';
+//             answer = false;
+//         }
+//         $('li.ball-' + questionNumber + ' img').attr('src', ball);
+//         return answer;
+//     }
+
+//     function resetScore() {
+//         $('ul.tennis-balls').empty();
+//         for (var i = 0; i < questions.length; i++) {
+//             $('ul.tennis-balls').append('<li class="score-ball ball-' +
+//                 i +
+//                 '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>'
+//             );
+//         }
+//     }
+
+//     function scrollToTop() {
+//         $(document).scrollTop(0);
+//     }
+// });
